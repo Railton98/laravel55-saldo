edd8db38a8655bffb3ae1f1eb773341044beac4b
Railton's Teck <railtonleal98@gmail.com>
2018-01-13 03:45:33 -0300

    Fazendo recarga e exibindo retorno

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -8,8 +8,20 @@ class Balance extends Model
 {
     public $timestamps = false;
 
-    public function deposit($value)
+    public function deposit(float $value) : Array
     {
-      dd($value);
+      $this->amount += number_format($value, 2, '.', '');
+      $deposit = $this->save();
+
+      if ($deposit)
+        return [
+          'success' => true,
+          'message' => 'Sucesso ao Recarregar'
+        ];
+
+      return [
+        'success' => false,
+        'message' => 'Falha ao Recarregar'
+      ];
     }
 }
