fa4aab41463458b10c8666917cc387ceb01bc75e
Railton's Teck <railtonleal98@gmail.com>
2018-01-13 03:04:57 -0300

    Criando formulário de Recarga

--- app/Http/Controllers/Admin/BalanceController.php
+++ app/Http/Controllers/Admin/BalanceController.php
@@ -11,7 +11,17 @@ class BalanceController extends Controller
     {
       $balance = auth()->user()->balance;
       $amount = $balance ? $balance->amount : 0;
-      
+
       return view('admin.balance.index', compact('amount'));
+    }
+
+    public function deposit()
+    {
+      return view('admin.balance.deposit');
+    }
+
+    public function depositStore(Request $request)
+    {
+      dd($request->all());
     }
 }
