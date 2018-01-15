360e07f0d7d13f7c24e479309755579e530d4020
Railton's Teck <railtonleal98@gmail.com>
2018-01-15 12:26:14 -0300

    Preparando Fazer Saque

--- resources/views/admin/balance/index.blade.php
+++ resources/views/admin/balance/index.blade.php
@@ -14,7 +14,9 @@
     <div class="box">
       <div class="box-header">
         <a href="{{ route('balance.deposit') }}" class="btn btn-primary"> <i class="fa fa-cart-plus" aria-hidden="true"></i> Recarregar</a>
-        <a href="" class="btn btn-danger"> <i class="fa fa-cart-arrow-down"></i> Sacar</a>
+        @if ( $amount > 0 )
+          <a href="{{ route('balance.withdrawn') }}" class="btn btn-danger"> <i class="fa fa-cart-arrow-down"></i> Sacar</a>
+        @endif
       </div>
       <div class="box-body">
 
--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,16 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function () {
-    Route::post('deposit', 'BalanceController@depositStore')->name('deposit.store');
-    Route::get('deposit', 'BalanceController@deposit')->name('balance.deposit');
-    Route::get('balance', 'BalanceController@index')->name('admin.balance');
-    Route::get('/', 'AdminController@index')->name('admin.home');
-});
+		Route::post('withdrawn', 'BalanceController@withdrawnStore')->name('withdrawn.store');
+		Route::get('withdrawn', 'BalanceController@withdrawn')->name('balance.withdrawn');
+		Route::post('deposit', 'BalanceController@depositStore')->name('deposit.store');
+		Route::get('deposit', 'BalanceController@deposit')->name('balance.deposit');
+		Route::get('balance', 'BalanceController@index')->name('admin.balance');
+		Route::get('/', 'AdminController@index')->name('admin.home');
+	});
 
 Route::get('/', 'Site\SiteController@index')->name('home');
 
