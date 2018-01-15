3b33eb774e822487c537b8ddaaaac606d6274498
Railton's Teck <railtonleal98@gmail.com>
2018-01-15 02:11:16 -0300

    Validando Entrada de Saldo

--- app/Http/Controllers/Admin/BalanceController.php
+++ app/Http/Controllers/Admin/BalanceController.php
@@ -2,28 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\MoneyValidationFormRequest;
 use App\Models\Balance;
 
-class BalanceController extends Controller
-{
-    public function index()
-    {
-      $balance = auth()->user()->balance;
-      $amount = $balance ? $balance->amount : 0;
+class BalanceController extends Controller {
 
-      return view('admin.balance.index', compact('amount'));
-    }
+	public function index() {
+		$balance = auth()->user()->balance;
+		$amount  = $balance?$balance->amount:0;
 
-    public function deposit()
-    {
-      return view('admin.balance.deposit');
-    }
+		return view('admin.balance.index', compact('amount'));
+	}
 
-    public function depositStore(Request $request)
-    {
-      $balance = auth()->user()->balance()->firstOrCreate([]);
-      $balance->deposit($request->value);
-    }
+	public function deposit() {
+		return view('admin.balance.deposit');
+	}
+
+	public function depositStore(MoneyValidationFormRequest $request) {
+		$balance = auth()->user()->balance()->firstOrCreate([]);
+		$balance->deposit($request->value);
+	}
 }
