6d40436a1b01bc48234a3f41baf1d7805b41ce6a
Railton's Teck <railtonleal98@gmail.com>
2018-01-24 01:03:35 -0300

    Exibindo Histórico Financeiro do Usuário

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,23 @@
 |
  */
 
-Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function () {
+$this->group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function () {
 
-		Route::post('transfer', 'BalanceController@transferStore')->name('transfer.store');
-		Route::post('confirm-transfer', 'BalanceController@confirmTransfer')->name('confirm-transfer');
-		Route::get('transfer', 'BalanceController@transfer')->name('balance.transfer');
+		$this->get('historic', 'BalanceController@historic')->name('admin.historic');
 
-		Route::post('withdraw', 'BalanceController@withdrawStore')->name('withdraw.store');
-		Route::get('withdraw', 'BalanceController@withdraw')->name('balance.withdraw');
+		$this->post('transfer', 'BalanceController@transferStore')->name('transfer.store');
+		$this->post('confirm-transfer', 'BalanceController@confirmTransfer')->name('confirm-transfer');
+		$this->get('transfer', 'BalanceController@transfer')->name('balance.transfer');
 
-		Route::post('deposit', 'BalanceController@depositStore')->name('deposit.store');
-		Route::get('deposit', 'BalanceController@deposit')->name('balance.deposit');
+		$this->post('withdraw', 'BalanceController@withdrawStore')->name('withdraw.store');
+		$this->get('withdraw', 'BalanceController@withdraw')->name('balance.withdraw');
 
-		Route::get('balance', 'BalanceController@index')->name('admin.balance');
+		$this->post('deposit', 'BalanceController@depositStore')->name('deposit.store');
+		$this->get('deposit', 'BalanceController@deposit')->name('balance.deposit');
 
-		Route::get('/', 'AdminController@index')->name('admin.home');
+		$this->get('balance', 'BalanceController@index')->name('admin.balance');
+
+		$this->get('/', 'AdminController@index')->name('admin.home');
 	});
 
 Route::get('/', 'Site\SiteController@index')->name('home');
